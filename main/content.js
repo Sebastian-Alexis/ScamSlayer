bb7e5c0625cde63d0ee39d5d286ece2a7d84dcad
Sebastian-Alexis <s3b3999@gmail.com>
2023-10-14 17:17:30 -0700

    UPDATED content.js and popup.html for logging

--- main/content.js
+++ main/content.js
@@ -37,8 +37,18 @@ const observer = new MutationObserver((mutations) => {
 
 // After detecting a suspicious popup...
 chrome.storage.local.get(["counter"], function (result) {
+  if (chrome.runtime.lastError) {
+    console.error("Error fetching counter:", chrome.runtime.lastError);
+    return;
+  }
   let count = result.counter || 0;
-  chrome.storage.local.set({ counter: count + 1 });
+  chrome.storage.local.set({ counter: count + 1 }, function () {
+    if (chrome.runtime.lastError) {
+      console.error("Error setting counter:", chrome.runtime.lastError);
+    } else {
+      console.log("Updated counter value:", count + 1);
+    }
+  });
 });
 
 observer.observe(document.body, {
