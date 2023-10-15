6b04cd0de9b8b2ec710787e37f8e9db7f07b765f
Sebastian-Alexis <s3b3999@gmail.com>
2023-10-14 17:02:23 -0700

    UPDATED content.js with more logging

--- main/content.js
+++ main/content.js
@@ -1,26 +1,33 @@
-const SCAM_KEYWORDS = ['malware', 'virus', 'infected'];
+console.log("ScamSlayer extension loaded and active.");
+
+const SCAM_KEYWORDS = ["malware", "virus", "infected"];
 
 // Function to check if a string contains any of the scam keywords
 function containsScamKeywords(text) {
-  return SCAM_KEYWORDS.some(keyword => text.toLowerCase().includes(keyword));
+  return SCAM_KEYWORDS.some((keyword) => text.toLowerCase().includes(keyword));
 }
 
 // Listen for the creation of new elements in the DOM
-const observer = new MutationObserver(mutations => {
-  console.log('Mutation observed');  // Logging
+const observer = new MutationObserver((mutations) => {
+  console.log("Mutation observed"); // Logging
 
-  mutations.forEach(mutation => {
+  mutations.forEach((mutation) => {
     if (mutation.addedNodes && mutation.addedNodes.length > 0) {
-      console.log('New nodes added');  // Logging
+      console.log("New nodes added"); // Logging
 
       for (let node of mutation.addedNodes) {
         if (node.nodeType === Node.ELEMENT_NODE) {
-          console.log(`Checking element: ${node.nodeName}`);  // Logging
+          console.log(`Checking element: ${node.nodeName}`); // Logging
 
           // If it's a popup (like a modal) check its content
-          if (node instanceof HTMLElement && containsScamKeywords(node.innerText)) {
-            console.log('Suspicious content detected');  // Logging
-            alert('Warning: This popup may be suspicious. Remember, you are in Chrome and it's likely from the website, not your computer.');
+          if (
+            node instanceof HTMLElement &&
+            containsScamKeywords(node.innerText)
+          ) {
+            console.log("Suspicious content detected"); // Logging
+            alert(
+              "Warning: This popup may be suspicious. Remember, you are in Chrome and its likely from the website, not your computer."
+            );
           }
         }
       }
@@ -30,5 +37,5 @@ const observer = new MutationObserver(mutations => {
 
 observer.observe(document.body, {
   childList: true,
-  subtree: true
+  subtree: true,
 });
